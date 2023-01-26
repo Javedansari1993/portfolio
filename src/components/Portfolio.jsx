4f8491824b94efeb0ea46a5bb7110d6206d636e0
Javed <javed.cabm@gmail.com>
2023-01-27 02:36:21 +0530

    add portfolio file

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,45 +11,51 @@ const Portfolio = () => {
     {
       id: 1,
       src: arrayDestruct,
+      heading:"Cryptocurrency Dashboard",
       demoLink:"https://javed-crypto-dashboard.netlify.app/",
       githubLink:"https://github.com/Javedansari1993/Crypto-dashboard",
       about:
-        "This dashboard using Reactjs provides information about your favourite Crypto Currencies, their trends and past prices in the form of a well organised graph including Line Chart as well as Bar Charts.",
+        "This dashboard using Reactjs provides information about your favourite Crypto Currencies, their trends and past prices in the form of a well organised.",
     },
     {
       id: 2,
       src: reactParallax,
+      heading:"E-commerce App",
       demoLink:"https://javed-ecommerce.netlify.app/",
       githubLink:"https://github.com/Javedansari1993/e-commarce",
-      about:"This E-commerce App using Reactjs you can add and remove your product where you can see all product added with total price in your Card tab all data are dummy these come from API",
+      about:"This E-commerce App using Reactjs you can add and remove your product where you can see all product added with total price in your Card.",
     },
     {
       id: 3,
       src: navbar,
+      heading:"Quiz App",
       demoLink:"https://javed-qiuz-app.netlify.app/",
       githubLink:"https://github.com/Javedansari1993/Quiz_app",
-      about:"This Quiz-App using Reactjs you can play quiz and having a functionality such as reset quiz,save and next, and exit from the quiz and if completed the quiz then you can see your over all marks on screen "
+      about:"This Quiz-App using Reactjs you can play quiz and having a functionality such as reset quiz,save and next, and exit from the quiz,at the end display final score on screen"
     },
     {
       id: 4,
       src: reactSmooth,
+      heading:"Get Youtube-Subscriber",
       demoLink:"https://drive.google.com/file/d/1jYGspRzzJojKfMVnDZwXkXyykVc73r2b/view?usp=sharing",
       githubLink:"https://github.com/Javedansari1993/Youtube-Subscriber",
-      about:"This project using NodeJS, Express, MongoDB Atlas database, Mongoose and dotenv for setting Environment varialbles and including perticular routes end point in the URL, through this users can access all subscribers."
+      about:"This project using NodeJS, Express, MongoDB Atlas database, Mongoose and dotenv for setting Environment varialbles."
     },
     {
       id: 5,
       src: installNode,
+      heading:"Blog-App",
       demoLink:"https://github.com/Javedansari1993/Express-blog-A",
       githubLink:"https://github.com/Javedansari1993/Express-blog-A",
-      about:"This project using NodeJS, Express, MongoDB Atlas database, Mongoose and you can create a custome blog where you can see all blog with diffrent diffrent routes and you see your contact and aboute page also",
+      about:"This project using NodeJS, Express, MongoDB Atlas database, Mongoose and you can create a custome blog where you can see all blog.",
     },
     {
       id: 6,
       src: reactWeather,
+      heading:"Workout-buddy-app",
       demoLink:"https://github.com/Javedansari1993/Mern-stack-Workout-buddy",
       githubLink:"https://github.com/Javedansari1993/Mern-stack-Workout-buddy",
-      about:"This project using Reactjs for fort-End and for Back-End using NodeJS, Express, Mongoose and users can access all workouts,access particular workouts, add workouts, delete, update workouts by perticular IDs."
+      about:"This project using Reactjs for fort-End and for Back-End using NodeJS, Express, Mongoose and users can access all workouts."
     },
   ];
 
@@ -66,9 +72,10 @@ const Portfolio = () => {
           <p className="py-6">Check out some of my work right here</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src ,about,demoLink,githubLink }) => (
+          {portfolios.map(({ id, src ,about,demoLink,githubLink,heading}) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <p className="text-white p-4 max-w-md">{about}</p>
+              <h1 className="text-white text-xl px-2 mx-4 max-w-md">{heading}</h1>
+              <p className="text-gray-400 p-4 max-w-md">{about}</p>
               <img
                 src={src}
                 alt=""
